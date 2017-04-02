5c28ba586f9c7c426ec18a425c0f73c74ed46ed6
Saulius Lukauskas <luksaulius@gmail.com>
2017-04-02 15:53:43 +0100

    initial dump

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='bokehselectize',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.0.1',
+
+    description='Selectize.js integration with Bokeh',
+    long_description='Selectize.js integration with Bokeh',
+
+    # The project's main homepage.
+    url='https://github.com/lukauskas/bokeh-selectize',
+
+    # Author details
+    author='Saulius Lukauskas',
+    author_email='saulius.lukauskas13@imperial.ac.uk',
+
+    # Choose your license
+    license='MIT',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    install_requires=['bokeh'],
+
+    extras_require={
+        'test': ['nose'],
+    },
+    package_data={
+    },
+    entry_points={
+    },
+)
